a847d0996b76e5bedc09f3978886ac3865b383be
Jose Velasco <jose.velasc.home@gmail.com>
2021-07-01 20:33:20 +0200

    add ch16

---
+++ src/ch16/Ex06.hs
@@ -0,0 +1,14 @@
+module Ex06 where
+
+data Tree = Leaf Int | Node Tree Tree 
+            deriving Show
+
+countLeaves :: Tree -> Int
+countLeaves (Leaf _)   = 1
+countLeaves (Node l r) = countLeaves l + countLeaves r
+
+countNodes :: Tree -> Int
+countNodes (Leaf _)   = 0
+countNodes (Node l r) = countNodes l + countNodes r + 1
+
+
